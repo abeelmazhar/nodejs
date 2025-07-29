0b337cca8e6256ddb2010bb078a0a7b26a809004
AbeelMazhar <abeelmazhar4@gmail.com>
2025-07-29 22:47:32 +0500

    comment out getAllUsers function and its export

--- controllers/getAllUsers.js
+++ controllers/getAllUsers.js
@@ -2,13 +2,13 @@ const User = require("../models/User");
 
 // GET /api/users
 const getAllUsers = async (req, res) => {
-  try {
-    const users = await User.find({}, "-password"); // exclude password
-    return res.status(200).json({ users });
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    return res.status(500).json({ message: "Server error" });
-  }
-};
+//   try {
+//     const users = await User.find({}, "-password"); // exclude password
+//     return res.status(200).json({ users });
+//   } catch (error) {
+//     console.error("Error fetching users:", error);
+//     return res.status(500).json({ message: "Server error" });
+//   }
+// };
 
-module.exports = { getAllUsers };
+// module.exports = { getAllUsers };
