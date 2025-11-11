d5af6cb5279d2f119922b0f5da0c317d016baa3e
Abeel mazhar <abeelmazhar4@gmail.com>
2025-11-11 09:51:50 +0500

    Reinstate user registration logic in signup controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,24 +18,24 @@ const signup = async (req, res) => {
     return res.status(400).json({ errors });
   }
 
-//   try {
-//     const userExists = await User.findOne({ email });
-//     if (userExists) {
-//       return res.status(409).json({ message: "Email already registered" });
-//     }
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(409).json({ message: "Email already registered" });
+    }
 
-//     const hashedPassword = await bcrypt.hash(password, 10);
-//     const newUser = new User({ name, email, password: hashedPassword });
-//     await newUser.save();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
 
-//     return res.status(201).json({
-//       message: "User registered",
-//       user: { id: newUser._id, name: newUser.name, email: newUser.email },
-//     });
-//   } catch (error) {
-//     return res.status(500).json({ message: "Server error" });
-//   }
-// };
+    return res.status(201).json({
+      message: "User registered",
+      user: { id: newUser._id, name: newUser.name, email: newUser.email },
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Server error" });
+  }
+};
 
 // Login controller
 const login = async (req, res) => {
